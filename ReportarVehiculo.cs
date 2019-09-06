44a36a0cce31b3f726e4a9379dd94e8e9215a2de
DannySilva99 <54969800+DannySilva99@users.noreply.github.com>
2019-09-05 20:20:05 -0700

    Add files via upload

---
+++ ReportarVehiculo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculos
+{
+    class ReportarVehiculo
+    {
+        public void Ver_Vehiculo(Vehiculos Carros)
+        {
+            Console.WriteLine("Numero de auto: {0}-" +
+                " Nombre: {1}-" +
+                " Marca {2}-" +
+                " Tipo {3}-" +
+                " Año {4}-", Carros.ID, Carros.Nombre, Carros.Marca, Carros.Tipo, Carros.Ano);
+        }
+        public void Enlistar_Vehiculo(Vehiculos[] c)
+        {
+            foreach (Vehiculos Carros in c)
+            {
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine(" Numero de auto: {0}-" +
+                " Nombre: {1}-" +
+                " Marca {2}-" +
+                " Tipo {3}-" +
+                " Año {4}-", Carros.ID, Carros.Nombre, Carros.Marca, Carros.Tipo, Carros.Ano);
+            }
+        }
+    }
+}
---
+++ Vehiculos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculos
+{
+    class Vehiculos
+    {
+        private int id;
+        private string nombre;
+        private string marca;
+        private string tipo;
+        private int año;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+        public string Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+        public int Ano
+        {
+            get { return año; }
+            set { año = value; }
+        }
+    }
+}
